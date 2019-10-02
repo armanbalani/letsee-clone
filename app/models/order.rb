bba57ebe7b32bb1adc481a627ae491dfcea8a62c
Arman Balani <arubalani@gmail.com>
2019-10-02 18:25:24 +0200

    added associations to models

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,5 @@
 class Order < ApplicationRecord
   belongs_to :user
+  has_many :carts, dependent: :destroy
+  has_many :reviews, dependent: :destroy
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,4 @@
 class Review < ApplicationRecord
   belongs_to :order
+  belongs_to :product
 end
