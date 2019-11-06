56187fc812d37cfaeac2f396e9dc8bc4a3134ede
Arman Balani <arubalani@gmail.com>
2019-11-06 14:00:52 +0100

    tried to do 2 rows with navbar

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get '/products/search', to: 'products#search'
   get '/profile/sell', to: 'dashboard#index'
+  get '/categories/index', to: 'categories#index'
 end
